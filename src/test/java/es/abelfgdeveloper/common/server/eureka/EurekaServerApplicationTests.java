49d10783bdaaae42a4ab8840d92da7d92a834dbc
Abel <abeldevelop@users.noreply.github.com>
2021-03-08 19:40:47 +0100

    Create the eureka server

---
+++ src/test/java/es/abelfgdeveloper/common/server/eureka/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.abelfgdeveloper.common.server.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
